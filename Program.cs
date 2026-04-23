367cb25745a8164f9072da92fa1bf230073636d4
josetpm <perezm.josetomas@gmail.com>
2026-04-23 12:51:23 +0200

    local fix

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<StudentService>();
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://judoclubapi.onrender.com")
+    BaseAddress = new Uri("https://judoclubapi.onrender.com","http://localhost:5187")
 });
 
 await builder.Build().RunAsync();
